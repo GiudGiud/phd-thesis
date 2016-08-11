f65020ec208141d5f0aab13749576416eba4662c
Will Boyd <wbinventor@gmail.com>
2016-08-10 21:40:00 -0400

    Added initial set of scripts for Chap. 8 on quantification

---
+++ data/quantification/2x2/build-xml.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+import openmc
+import openmc.opencg_compatible as opencg_compatible
+import infermc.beavrs
+from infermc.energy_groups import group_structures
+
+
+####################   User-specified Simulation Parameters  ###################
+
+batches = 1000
+inactive = 100
+particles = 1000000
+
+
+#########   Exporting to OpenMC geometry.xml and materials.xml Files  ##########
+
+# Write all BEAVRS materials to materials.xml file
+infermc.beavrs.make_iso_in_lab()
+infermc.beavrs.write_materials_file()
+
+# Extract reflected geometry from InferMC's pre-built assembly Geometries
+two_by_two = infermc.beavrs.build_two_by_two('Fuel 1.6% enr instr no BAs',
+                                             'Fuel 3.1% enr instr 20')
+openmc_geometry = opencg_compatible.get_openmc_geometry(two_by_two)
+openmc_geometry.export_to_xml()
+
+##################   Exporting to OpenMC settings.xml File  ###################
+
+# Construct uniform initial source distribution over fissionable zones
+lower_left = two_by_two.bounds[:3]
+upper_right = two_by_two.bounds[3:]
+source = openmc.source.Source(space=openmc.stats.Box(lower_left, upper_right))
+source.space.only_fissionable = True
+
+settings_file = openmc.Settings()
+settings_file.batches = batches
+settings_file.inactive = inactive
+settings_file.particles = particles
+settings_file.statepoint_interval = 10
+settings_file.ptables = True
+settings_file.output = {'tallies': False}
+settings_file.source = source
+settings_file.sourcepoint_write = False
+settings_file.export_to_xml()
+
+
+########################   Build OpenMC MGXS Library  #########################
+
+# Get all cells filled with a "fuel" material
+mat_cells = openmc_geometry.get_all_material_cells()
+fuel_cells = []
+for cell in mat_cells:
+    if 'fuel' in cell.fill.name.lower():
+        fuel_cells.append(cell)
+
+# Initialize a fine (70-) group "distribcell" MGXS Library for OpenMOC
+cell_mgxs_lib = openmc.mgxs.Library(openmc_geometry, by_nuclide=True)
+cell_mgxs_lib.energy_groups = group_structures['CASMO']['70-group']
+cell_mgxs_lib.mgxs_types = ['total', 'fission', 'nu-fission', 'nu-scatter matrix',
+                            'chi', 'absorption', 'capture']
+cell_mgxs_lib.domain_type = 'distribcell'
+cell_mgxs_lib.domains = fuel_cells
+cell_mgxs_lib.correction = None
+cell_mgxs_lib.build_library()
+
+# Initialize a fine (70-) group "material" MGXS Library for OpenMOC
+mat_mgxs_lib = openmc.mgxs.Library(openmc_geometry, by_nuclide=True)
+mat_mgxs_lib.energy_groups = group_structures['CASMO']['70-group']
+mat_mgxs_lib.mgxs_types = ['total', 'fission', 'nu-fission', 'nu-scatter matrix',
+                           'chi', 'absorption', 'capture']
+mat_mgxs_lib.domain_type = 'material'
+mat_mgxs_lib.correction = None
+mat_mgxs_lib.build_library()
+
+
+###################  Create Mesh Tallies for Verification  ####################
+
+# Instantiate a tally Mesh
+mesh = openmc.Mesh(name='assembly mesh')
+mesh.type = 'regular'
+mesh.dimension = [34, 34, 1]
+mesh.lower_left = lower_left
+mesh.width = (np.array(upper_right) - np.array(lower_left))
+mesh.width[:2] /= 34
+
+# Instantiate tally Filter
+mesh_filter = openmc.Filter()
+mesh_filter.mesh = mesh
+
+# Instantiate energy-integrated fission rate mesh Tally
+fission_rates = openmc.Tally(name='fission rates')
+fission_rates.filters = [mesh_filter]
+fission_rates.scores = ['fission']
+
+# Instantiate energy-wise U-238 capture rate mesh Tally
+capture_rates = openmc.Tally(name='u-238 capture')
+capture_rates.filters = [mesh_filter]
+capture_rates.nuclides = ['U-238']
+capture_rates.scores = ['absorption', 'fission']
+
+# Create a "tallies.xml" file for the mesh tallies
+tallies_file = openmc.Tallies([fission_rates, capture_rates])
+cell_mgxs_lib.add_to_tallies_file(tallies_file, merge=True)
+mat_mgxs_lib.add_to_tallies_file(tallies_file, merge=True)
+tallies_file.export_to_xml()
---
+++ data/quantification/plot-capt-rates.py
@@ -0,0 +1,79 @@
+import os
+import copy
+import openmc
+import numpy as np
+import matplotlib.pyplot as plt
+from _collections import OrderedDict
+
+###############################################################################
+# SINGLE FUEL ASSEMBLIES
+###############################################################################
+
+directories = OrderedDict({'fuel-1.6': '1.6% Enr. (no BPs)',
+                           'fuel-3.1':'3.1% Enr. (no BPs)',
+                           'fuel-3.1-20BPs': '3.1% Enr. (20 BPs)',
+                           '2x2': '2x2 Colorset',
+                           'reflector': '2x2 Colorset w/ Reflector',
+                           'full-core': 'Full Core'})
+
+for directory in directories:
+    sp = openmc.StatePoint(os.path.join(directory, 'statepoint.1000.h5'))
+    batches = np.linspace(1, sp.current_batch, sp.current_batch, dtype=np.int)
+
+    # Extract mesh for mesh tallies from OpenMC StatePoint
+    openmc_mesh = sp.meshes[10000]
+
+    # Extract OpenMC capture rate mesh tally from StatePoint
+    capt = sp.get_tally(name='u-238 capture')
+    absorb = capt.get_slice(scores=['absorption'])
+    fission = capt.get_slice(scores=['fission'])
+    capt = absorb - fission
+
+    # Copy and reshape the NumPy array of mean values
+    capt_mean = copy.deepcopy(capt.mean)
+    capt_mean.shape = tuple(openmc_mesh.dimension)
+    capt_mean = np.squeeze(capt_mean)
+    capt_mean = np.fliplr(capt_mean)
+
+    # Copy and reshape the NumPy array of standard deviation values
+    capt_std_dev = copy.deepcopy(capt.std_dev)
+    capt_std_dev.shape = tuple(openmc_mesh.dimension)
+    capt_std_dev = np.squeeze(capt_std_dev)
+    capt_std_dev = np.fliplr(capt_std_dev)
+
+    # Normalize the capture rates to sum to unity
+    capt_mean /= np.mean(np.ravel(capt_mean[capt_mean != 0.]))
+
+    # Set zero capture rates to NaN for transparency in plots
+    zero_indices = np.where(capt_mean < 1E-5)
+    capt_mean[zero_indices] = np.nan
+    capt_std_dev[zero_indices] = np.nan
+
+    # Compute the OpenMC relative error from the non-normalized mean
+    rel_err = capt_std_dev / capt_mean * 100.
+
+    # Instantiate a color mapping scheme which makes NaNs transparent
+    cmap = plt.get_cmap('jet')
+    cmap.set_bad(alpha=0.0)
+
+    # Create a matplotlib figure for mean capture rates
+    fig = plt.figure()
+    plt.imshow(capt_mean, interpolation='none', cmap=cmap,
+               vmin=np.nanmin(capt_mean), vmax=np.nanmax(capt_mean))
+    cbar = plt.colorbar(format='%1.3f')
+    cbar.ax.ticklabel_format(fontsize=20)
+    plt.title('Normalized U-238 Capture Rates', fontsize=20)
+    plt.grid(False)
+    plt.axis('off')
+    plt.savefig('capt-mean-' + directory.replace('.', '') + '.png', bbox_inches='tight')
+
+    # Create a matplotlib figure for rel. err. fission rates
+    fig = plt.figure()
+    plt.imshow(rel_err, interpolation='none', cmap=cmap,
+               vmin=np.nanmin(rel_err), vmax=np.nanmax(rel_err))
+    cbar = plt.colorbar(format='%1.2e')
+    cbar.ax.ticklabel_format(fontsize=20)
+    plt.title('U-238 Capture Rate Relative Error [%]', fontsize=20)
+    plt.grid(False)
+    plt.axis('off')
+    plt.savefig('capt-rel-err-' + directory.replace('.', '') + '.png', bbox_inches='tight')
