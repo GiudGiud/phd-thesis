c576d43153f3dc172737befc43816801cab5a720
Will Boyd <wbinventor@gmail.com>
2016-08-11 20:55:46 -0400

    Now using num of groups in output file name

--- data/quantification/assm-1.6/batchwise.py
+++ data/quantification/assm-1.6/batchwise.py
@@ -1,29 +1,44 @@
+import glob
 import types
 import infermc
 from discretize import discretize_geometry
 
-# Initialize a Batchwise to iterate over all statepoints
-# This requires the --batchwise={'perfect', 'adaptive'} command line argument
-batchwise = infermc.batchwise.Batchwise.get_batchwise()
-batchwise.cell_mgxslib_filename = 'distribcell'
-batchwise.mat_mgxslib_filename = 'material'
-batchwise.zcoord = 205.
-batchwise.log_level = 'INFO'
+statepoints = glob.glob('statepoint.*.h5')
 
-# Attach a method to discretize this geometry to the Batchwise instance
-batchwise._discretize_geometry = types.MethodType(discretize_geometry, batchwise)
+# FIXME: Loop over clusterizers then groups
+groups = [2, 8, 70]
+clusterizer_types = ['infinite', 'null', 'degenerate']
 
-# If a basic clusterizer type ('null', 'degenerate') was not specified
-# as a command line argument initialize a clusterizer with scikit-learn
-if batchwise.options.clusterizer_type == 'infinite':
-    clusterizer = infermc.clusterizer.NullClusterizer()
-    batchwise.clusterizer = clusterizer
-    batchwise.clusterizer._type = 'infinite'
-    batchwise.mat_mgxslib_directories.append('../pin-1.6/')
+for clusterizer_type in clusterizer_types:
+    # Initialize a Batchwise to iterate over all statepoints
+    batchwise = infermc.batchwise.PerfectBatchwise()
+    batchwise.sp_start = len(statepoints) - 1
+    batchwise.cell_mgxslib_filename = 'distribcell'
+    batchwise.mat_mgxslib_filename = 'material'
+    batchwise.zcoord = 205.
+    batchwise.log_level = 'INFO'
 
-# Turn off MGXS plotting for speed
-if batchwise.clusterizer:
+    # FIXME: Add report to add to table
+
+    # Attach a method to discretize this geometry to the Batchwise instance
+    batchwise._discretize_geometry = types.MethodType(discretize_geometry, batchwise)
+
+    if clusterizer_type == 'infinite':
+        clusterizer = infermc.clusterizer.NullClusterizer()
+        batchwise.clusterizer = clusterizer
+        batchwise.clusterizer._type = 'infinite'
+        batchwise.mat_mgxslib_directories.append('../pin-1.6/')
+    elif clusterizer_type == 'null':
+        batchwise.clusterizer = infermc.clusterizer.NullClusterizer()
+    elif clusterizer_type == 'degenerate':
+        batchwise.clusterizer = infermc.clusterizer.DegenerateClusterizer()
+
+    # Turn off MGXS plotting for speed
     batchwise.clusterizer.plot_mgxs = False
 
-# Execute OpenMOC simulations over all batches of clustered MGXS libraries
-batchwise.execute()
+    # Execute OpenMOC simulations over all batches of clustered MGXS libraries
+    for num_groups in groups:
+        batchwise.num_fine_groups = num_groups
+        batchwise.type = '{}-groups'.format(num_groups)
+        batchwise.execute()
+        batchwise._sp_counter -= 1
