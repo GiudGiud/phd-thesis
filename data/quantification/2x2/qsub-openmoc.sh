74d46e209994ed5365a7ce0fca96ce7f22e1ed13
Will Boyd <wbinventor@gmail.com>
2016-08-14 18:56:53 -0400

    Fixed bug in 2x2 PBS script

--- data/quantification/2x2/qsub-openmoc.sh
+++ data/quantification/2x2/qsub-openmoc.sh
@@ -2,12 +2,12 @@
 
 qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=2 --clusterizer=null" openmoc-2x2.pbs
 qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=2 --clusterizer=infinite" openmoc-2x2.pbs
-qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=2 --clusterizer=degenerate" openmoc-2x2.6.pbs
+qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=2 --clusterizer=degenerate" openmoc-2x2.pbs
 
 qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=8 --clusterizer=null" openmoc-2x2.pbs
 qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=8 --clusterizer=infinite" openmoc-2x2.pbs
-qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=8 --clusterizer=degenerate" openmoc-2x2.6.pbs
+qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=8 --clusterizer=degenerate" openmoc-2x2.pbs
 
 qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=70 --clusterizer=null" openmoc-2x2.pbs
 qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=70 --clusterizer=infinite" openmoc-2x2.pbs
-qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=70 --clusterizer=degenerate" openmoc-2x2.6.pbs
+qsub -P moose -v flags="-a 128 -s 0.05 --num-fine=70 --clusterizer=degenerate" openmoc-2x2.pbs
