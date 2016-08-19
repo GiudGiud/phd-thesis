00e5d51873fbc657d2d2d19ccbc32a82816b681f
Will Boyd <wbinventor@gmail.com>
2016-08-19 11:21:00 -0400

    Added full core CMFD image

--- data/quantification/full-core/run-openmoc.py
+++ data/quantification/full-core/run-openmoc.py
@@ -43,7 +43,7 @@ elif batchwise.options.clusterizer_type == 'degenerate':
 batchwise.clusterizer.plot_mgxs = False
 batchwise.plot_materials = True
 batchwise.plot_cells = True
-batchwise.plot_fsrs = True
+batchwise.plot_fsrs = False
 
 # Execute OpenMOC simulations over all batches of clustered MGXS libraries
 batchwise.execute()
