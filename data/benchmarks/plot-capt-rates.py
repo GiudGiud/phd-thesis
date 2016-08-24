Fixed calculation of rel. err. for OpenMC mesh tally reference cases

data/benchmarks/plot-capt-rates.py
@@ -29,6 +29,10 @@ for directory in directories:
     fission = capt.get_slice(scores=['fission'])
     capt = absorb - fission
 
+    # Normalize the capture rates to sum to unity
+    capt_mean = copy.deepcopy(capt.mean)
+    capt /= np.mean(np.ravel(capt_mean[capt_mean != 0.]))
+    
     # Copy and reshape the NumPy array of mean values
     capt_mean = copy.deepcopy(capt.mean)
     capt_mean.shape = tuple(openmc_mesh.dimension)
@@ -40,9 +44,6 @@ for directory in directories:
     capt_std_dev.shape = tuple(openmc_mesh.dimension)
     capt_std_dev = np.squeeze(capt_std_dev)
     capt_std_dev = np.fliplr(capt_std_dev)
-
-    # Normalize the capture rates to sum to unity
-    capt_mean /= np.mean(np.ravel(capt_mean[capt_mean != 0.]))
 
     # Set zero capture rates to NaN for transparency in plots
     zero_indices = np.where(capt_mean < 1E-5)

data/benchmarks/plot-fiss-rates.py
@@ -26,6 +26,10 @@ for directory in directories:
     # Extract OpenMC fission rate mesh tally from StatePoint
     fiss = sp.get_tally(name='fission rates')
 
+    # Normalize the fission rates to sum to unity
+    fiss_mean = copy.deepcopy(fiss.mean)
+    fiss /= np.nanmean(np.ravel(fiss_mean[fiss_mean != 0]))
+
     # Copy and reshape the NumPy array of mean values
     fiss_mean = copy.deepcopy(fiss.mean)
     fiss_mean.shape = tuple(openmc_mesh.dimension)
@@ -37,9 +41,6 @@ for directory in directories:
     fiss_std_dev.shape = tuple(openmc_mesh.dimension)
     fiss_std_dev = np.squeeze(fiss_std_dev)
     fiss_std_dev = np.fliplr(fiss_std_dev)
-
-    # Normalize the fission rates to sum to unity
-    fiss_mean /= np.nanmean(np.ravel(fiss_mean[fiss_mean != 0]))
 
     # Set zero fission rates to NaN for transparency in plots
     zero_indices = np.where(fiss_mean < 1E-3)